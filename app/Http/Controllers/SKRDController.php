6c96ba49ecef78eeed87f18835cd3883936a11ef
axxpxmd <asiphamdi@gmail.com>
2021-10-04 15:36:42 +0700

    update

--- app/Http/Controllers/SKRDController.php
+++ app/Http/Controllers/SKRDController.php
@@ -292,8 +292,8 @@ class SKRDController extends Controller
         if ($daysDiff > 0) {
             $clientRefnum = $no_bayar;
             $amount       = \strval((int) str_replace(['.', 'Rp', ' '], '', $request->jumlah_bayar));
-            // $expiredDate  = $request->tgl_skrd_awal . ' 23:59:59';
-            $expiredDate  = Carbon::now()->addMinute(1)->format('Y-m-d H:i:s');
+            $expiredDate  = $request->tgl_skrd_awal . ' 23:59:59';
+            // $expiredDate  = Carbon::now()->addMinute(1)->format('Y-m-d H:i:s');
             $customerName = $request->nm_wajib_pajak;
             $productCode  = $request->kd_jenis;
 
