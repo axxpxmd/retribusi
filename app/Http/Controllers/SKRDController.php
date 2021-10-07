e1620f52e4911f54fbdbf24b7509de6298829df1
axxpxmd <asiphamdi@gmail.com>
2021-10-07 17:05:34 +0700

    update

--- app/Http/Controllers/SKRDController.php
+++ app/Http/Controllers/SKRDController.php
@@ -548,8 +548,7 @@ class SKRDController extends Controller
 
         //* Tahap 2
         $input = $request->all();
-        $input = $request->except('kode_rekening');
-        $input = $request->except('kd_jenis');
+        $input = $request->except(['kode_rekening', 'kd_jenis']);
         $data->update($input);
         $data->update([
             'nomor_va_bjb' => $VABJB,
