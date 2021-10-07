de1abd2b144e8d4417742171d39e47dd7882c7b0
axxpxmd <asiphamdi@gmail.com>
2021-10-07 14:31:26 +0700

    update

--- resources/views/pages/skrd/create.blade.php
+++ resources/views/pages/skrd/create.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('title', '| '.$title.'')
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/myStyle.css') }}">
 <div class="page has-sidebar-left height-full">
     <header class="blue accent-3 relative nav-sticky">
         <div class="container-fluid text-white">
