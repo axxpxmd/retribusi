3a84618f8687035dd15af4a4c1bf8615bfee355f
Asip Hamdi <asiphamdi@gmail.com>
2022-03-23 14:40:46 +0700

    update

--- app/Http/Controllers/STRDController.php
+++ app/Http/Controllers/STRDController.php
@@ -88,7 +88,7 @@ class STRDController extends Controller
 
         return DataTables::of($data)
             ->addColumn('action', function ($p) {
-                $path_sftp = 'file_ttd_strd/';
+                $path_sftp = 'file_ttd_skrd/';
                 $fileName  = str_replace(' ', '', $p->nm_wajib_pajak) . '-' . $p->no_skrd . ".pdf";
 
                 if ($p->tgl_strd_akhir == null) {
