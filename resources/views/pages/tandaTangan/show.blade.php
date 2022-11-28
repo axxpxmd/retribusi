028273f591d02827622f3d1d20c8cfaef8549edc
Asip Hamdi <asiphamdi@gmail.com>
2022-11-28 13:37:43 +0700

    update

--- resources/views/pages/tandaTangan/show.blade.php
+++ resources/views/pages/tandaTangan/show.blade.php
@@ -282,7 +282,10 @@
                 <div class="card">
                     <h6 class="card-header font-weight-bold">Tanda Tangan</h6>
                     <div class="card-body">
-                        <form class="needs-validation" method="POST" action="{{ route('tanda-tangan.tandaTangan') }}" enctype="multipart/form-data" novalidate>
+                        {{-- <form class="needs-validation" method="POST" action="{{ route('tanda-tangan.tandaTangan') }}" enctype="multipart/form-data" novalidate>
+                            {{ method_field('POST') }}
+                            {{ csrf_field() }}  --}}
+                        <form class="needs-validation" method="POST" action="{{ route('tanda-tangan.tteBackup') }}" enctype="multipart/form-data" novalidate>
                             {{ method_field('POST') }}
                             {{ csrf_field() }} 
                             <div class="text-center row">
