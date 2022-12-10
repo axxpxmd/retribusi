e0fe1953a795c2471c3d58989ea57972b3b880d3
Asip Hamdi <asiphamdi@gmail.com>
2022-12-10 13:49:25 +0700

    update

--- resources/views/pages/datawp/show.blade.php
+++ resources/views/pages/datawp/show.blade.php
@@ -30,47 +30,63 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card mt-2">
-                            <h6 class="card-header"><strong>Data Pemohon Retribusi</strong></h6>
+                            <h6 class="card-header font-weight-bold">Data Pemohon Retribusi</h6>
                             <div class="card-body">
                                 <div class="col-md-12">
                                     <div class="row">
-                                        <label class="col-md-2 text-right s-12"><strong>OPD :</strong></label>
+                                        <label class="col-md-2 text-right s-12 font-weight-bold">OPD :</label>
                                         <label class="col-md-8 s-12">{{ $data->opd->n_opd }}</label>
                                     </div>
                                     <div class="row">
-                                        <label class="col-md-2 text-right s-12"><strong>Jenis Pendapatan :</strong></label>
+                                        <label class="col-md-2 text-right s-12 font-weight-bold">Jenis Pendapatan :</label>
                                         <label class="col-md-8 s-12">{{ $data->jenis_pendapatan->jenis_pendapatan }}</label>
                                     </div>
                                     <div class="row">
-                                        <label class="col-md-2 text-right s-12"><strong>Rincian Jenis Pendapatan :</strong></label>
+                                        <label class="col-md-2 text-right s-12 font-weight-bold">Rincian Jenis Pendapatan :</label>
                                         <label class="col-md-8 s-12">{{ $data->rincian_jenis->rincian_pendapatan }}</label>
                                     </div>
                                     <hr>
                                     <div class="row">
-                                        <label class="col-md-2 text-right s-12"><strong>Nama :</strong></label>
+                                        <label class="col-md-2 text-right s-12 font-weight-bold">Nama :</label>
                                         <label class="col-md-8 s-12">{{ $data->nm_wajib_pajak }}</label>
                                     </div>
                                     <div class="row">
-                                        <label class="col-md-2 text-right s-12"><strong>Alamat:</strong></label>
+                                        <label class="col-md-2 text-right s-12 font-weight-bold">Alamat :</label>
                                         <label class="col-md-8 s-12">{{ $data->alamat_wp }}</label>
                                     </div>
                                     <div class="row">
-                                        <label class="col-md-2 text-right s-12"><strong>Lokasi :</strong></label>
+                                        <label class="col-md-2 text-right s-12 font-weight-bold">Lokasi :</label>
                                         <label class="col-md-8 s-12">{{ $data->lokasi }}</label>
                                     </div>
                                     <div class="row">
-                                        <label class="col-md-2 text-right s-12"><strong>Kecamatan :</strong></label>
+                                        <label class="col-md-2 text-right s-12 font-weight-bold">Kecamatan :</label>
                                         <label class="col-md-8 s-12">{{ $data->kecamatan->n_kecamatan }}</label>
                                     </div>
                                     <div class="row">
-                                        <label class="col-md-2 text-right s-12"><strong>Kelurahan :</strong></label>
+                                        <label class="col-md-2 text-right s-12 font-weight-bold">Kelurahan :</label>
                                         <label class="col-md-8 s-12">{{ $data->kelurahan->n_kelurahan }}</label>
                                     </div>
-                                      <div class="row">
-                                        <label class="col-md-2 text-right s-12"><strong>Total Retribusi :</strong></label>
-                                        <label class="col-md-8 s-12">{{ $data->totalRetribusi->count() }}</label>
+                                    <div class="row">
+                                        <label class="col-md-2 text-right s-12 font-weight-bold">Total Retribusi :</label>
+                                        <label class="col-md-8 s-12">{{ $data->totalRetribusi->count() }} Retribusi</label>
                                     </div>
                                     <div class="row">
+                                        <label class="col-md-2 text-right s-12"></label>
+                                        <label class="col-md-8 s-12">
+                                            @foreach ($data->totalRetribusi as $i)
+                                                <li class="mt-2">
+                                                    <a target="_blank" href="{{ route('sts.show', Crypt::encrypt($i->id)) }}">{{ $i->no_skrd }}</a>
+                                                    &nbsp;&nbsp; ( {{ Carbon\Carbon::createFromFormat('Y-m-d', $i->tgl_skrd_awal)->format('d F Y') }} ) &nbsp;&nbsp;
+                                                    @if ($i->status_bayar)
+                                                        <span class="badge badge-success">Sudah Bayar</span>
+                                                    @else
+                                                        <span class="badge badge-danger">Belum Bayar</span>
+                                                    @endif
+                                                </li>
+                                            @endforeach
+                                        </label>
+                                    </div>
+                                    <div class="row mt-2">
                                         <label class="col-md-2 text-right s-12"></label>
                                         <label class="col-md-3 s-12">
                                             <a href="{{ route('skrd.create', array('data_wp_id' =>  Crypt::encrypt($data->id))) }}" class="btn btn-sm btn-primary"><i class="icon-arrow_forward mr-2"></i>Buat SKRD</a>
