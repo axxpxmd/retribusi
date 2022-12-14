98e68003a49d61ce2567129594914d9069dca64e
Asip Hamdi <asiphamdi@gmail.com>
2022-12-14 16:28:05 +0700

    update

--- resources/views/pages/tandaTangan/reportTTEskrd.blade.php
+++ resources/views/pages/tandaTangan/reportTTEskrd.blade.php
@@ -383,7 +383,6 @@
                         @else
                             <p>Tangerang Selatan, </p>
                         @endif
-                        @if ($data->status_ttd == 1 || $data->status_ttd == 3)
                         <table style="margin-left: 410px !important; margin-top: -8px; margin-bottom: -8px">
                             <tr class="a">
                                 <td style="padding: 1px" width="8%" class="a"> {!! $img !!}</td>
@@ -398,13 +397,6 @@
                                 </td>
                             </tr>
                         </table>
-                        @else
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        @endif
                         @if ($data->nm_ttd != null)
                             <p class="m-b-5 m-b-0"><u>{{ $data->nm_ttd }}</u></p>
                         @else
