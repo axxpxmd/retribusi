16c03e06c2359ddf74287033a3b45c91b202422a
Asip Hamdi <asiphamdi@gmail.com>
2023-01-24 10:46:03 +0700

    update

--- resources/views/pages/skrd/create.blade.php
+++ resources/views/pages/skrd/create.blade.php
@@ -102,9 +102,9 @@
                                             </div>
                                         </div>
                                         <div class="row mb-2">
-                                            <label for="no_telp" class="col-form-label s-12 col-sm-4 text-right">No Telp</label>
+                                            <label for="no_telp" class="col-form-label s-12 col-sm-4 text-right">No Telp<span class="text-danger ml-1">*</span></label>
                                             <div class="col-sm-8">
-                                                <input type="number" name="no_telp" id="no_telp" value="{{ $data_wp != null ? $data_wp->no_telp : '' }}" class="form-control r-0 s-12" autocomplete="off"/>
+                                                <input type="number" name="no_telp" id="no_telp" value="{{ $data_wp != null ? $data_wp->no_telp : '' }}" class="form-control r-0 s-12" autocomplete="off" required>
                                             </div>
                                         </div>
                                         <div class="row mb-1">
