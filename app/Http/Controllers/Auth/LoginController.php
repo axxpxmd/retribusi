401449945ed1556a76be498f924c1ea2480fbe3d
Asip Hamdi <asiphamdi@gmail.com>
2024-01-15 11:08:13 +0700

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -51,4 +51,9 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function username()
+    {
+        return 'username';
+    }
 }
