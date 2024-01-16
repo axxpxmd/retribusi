d0229b6d386fe4cea6a8c90e4af94dd63657d9ce
Asip Hamdi <asiphamdi@gmail.com>
2024-01-16 13:59:13 +0700

    update'

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -37,7 +37,7 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception)
     {
-        $logError = 'Message : ' . $exception->getMessage() . ' | File : ' . $exception->getFile() . ' | Line : ' . $exception->getLine() . ' | URL : ' . request()->url();
+        $logError = 'Message : ' . $exception->getMessage() . ' | File : ' . $exception->getFile() . ' | Line : ' . $exception->getLine() . ' | URL : ' . request()->url() . ' | IP : ' . request()->getClientIp();
         if (config('app.log_tebot') == 1) {
             Tebot::alert($logError)->channel('log_skrd');
         }
